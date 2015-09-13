178c8c22a983fe28e10d2d84b5818e1d5d380bb8
ComandaKOK <rmkaporin@gmail.com>
2015-09-13 17:41:10 +0300

    Added geocoder

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,12 +1,13 @@
 class Place < ActiveRecord::Base
   belongs_to :user
 
+  geocoded_by :address, :latitude => :latitude, :longitude => :longitude
+  after_validation :geocode 
+
   default_scope -> { order('created_at DESC') }
 
   validates :place,  	    presence: true,   length: { maximum: 50 }
   validates :description, 	presence: true
   validates :address,  		presence: true
   validates :user_id, presence: true
-  validates :latitude, presence: true
-  validates :longitude, presence: true
 end
